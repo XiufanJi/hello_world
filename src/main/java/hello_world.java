8174fbcde5aae8f9d517c06302299e63c59dbd03
Xiufanji <timegao@outlook.com>
2020-06-15 15:43:54 +0800

    add test message

--- src/main/java/hello_world.java
+++ src/main/java/hello_world.java
@@ -1,5 +1,6 @@
 public class hello_world {
     public static void main(String args[]){
-        System.out.print("hello world!");
+		System.out.println("just for test,let's start!");
+        System.out.println("hello world!");
     }
 }
